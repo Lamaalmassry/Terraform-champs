ded543b4daf7fba93b6bdad891dc5ecc3e08915d
Lama almassry <59393893+Lamaalmassry@users.noreply.github.com>
2024-06-07 10:33:23 +0300

    Add files via upload

---
+++ Week Two Workshop/main.tf
@@ -0,0 +1,52 @@
+
+resource "aws_s3_bucket" "terraformbucket" {
+    bucket = var.bucket_name
+
+    tags = {
+      "Enviorment" = var.Enviorment
+       "owner" =var.owner
+    }
+
+}
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraformbucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.terraformbucket.id
+
+
+rule {
+  apply_server_side_encryption_by_default {
+    sse_algorithm="AES256"
+}
+ 
+    bucket_key_enabled = true
+  }
+}
+
+
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
+  bucket = aws_s3_bucket.terraformbucket.id
+
+  rule {
+    id     = "expire object after 7 days"
+    status = "Enabled"
+
+    expiration {
+      days = 7
+    }
+  }
+}
+
+
+
+/*resource "aws_s3_bucket_object_lock_configuration" "objevtlock" {
+  bucket = aws_s3_bucket.terraformbucket.id
+  object_lock_enabled="Disabled"
+}*/
+
---
+++ Week Two Workshop/var.tf
@@ -0,0 +1,15 @@
+variable bucket_name{
+ type =string
+ default = "terraform-workshop-lama"
+}
+  variable "Enviorment" {
+    type = string
+    default = "terraformChamps"
+}
+
+variable "owner" {
+    type = string 
+    default = "lama"
+}
+
+   
