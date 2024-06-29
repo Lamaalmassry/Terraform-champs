c482e29b5a039b02800d69b8fd3643eb7345ef46
Lama almassry <59393893+Lamaalmassry@users.noreply.github.com>
2024-06-29 12:04:02 +0300

    Update postgresql.tf

--- Week Five Workshop/postgresql.tf
+++ Week Five Workshop/postgresql.tf
@@ -6,11 +6,14 @@ resource "aws_db_instance" "postgresql" {
   engine                  = "postgres"
   engine_version          = "15.4"
   username                = "postgres"
+  db_name                 = "postgresqldatabase"
   password                = "lama__592000"
   db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group_01.name
   vpc_security_group_ids  = [aws_security_group.sg_01.id]
   skip_final_snapshot     = true
- 
+  multi_az = false
+
+
   tags = {
     Name = "postgresqldatabase"
   }
