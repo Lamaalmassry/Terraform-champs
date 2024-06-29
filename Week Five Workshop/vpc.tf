4fd6b1e88d92cdc692277a68dca029f2044477bc
Lama almassry <59393893+Lamaalmassry@users.noreply.github.com>
2024-06-29 12:06:44 +0300

    Update vpc.tf

--- Week Five Workshop/vpc.tf
+++ Week Five Workshop/vpc.tf
@@ -1,8 +1,29 @@
-
 resource "aws_vpc" "vpc_01" {
   cidr_block = "10.0.0.0/16"
+ 
 
    tags = {
     Name="vpc_01"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc_01.id
+
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc_01.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id  
+  }
+
+  tags = {
+    Name = "public-route-table"
   }
 }
