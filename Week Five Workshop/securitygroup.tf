e91c093b5c3db53d61261433625b89fb356d5428
Lama almassry <59393893+Lamaalmassry@users.noreply.github.com>
2024-06-29 12:04:54 +0300

    Update securitygroup.tf

--- Week Five Workshop/securitygroup.tf
+++ Week Five Workshop/securitygroup.tf
@@ -1,4 +1,3 @@
-
 resource "aws_security_group" "sg_01" {
   vpc_id      = aws_vpc.vpc_01.id
 
@@ -7,6 +6,7 @@ resource "aws_security_group" "sg_01" {
     to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+     security_groups = [aws_security_group.bastion_sg.id]
   }
 
   egress {
@@ -18,5 +18,29 @@ resource "aws_security_group" "sg_01" {
 
     tags = {
     Name = "sg_01"
+  }
+}
+
+
+resource "aws_security_group" "bastion_sg" {
+  vpc_id = aws_vpc.vpc_01.id
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bastion_sg"
   }
 }
