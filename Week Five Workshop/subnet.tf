2bdf9f253ae4fb3fca1a2cd225b3efee16a9819f
Lama almassry <59393893+Lamaalmassry@users.noreply.github.com>
2024-06-29 12:07:24 +0300

    Update subnet.tf

--- Week Five Workshop/subnet.tf
+++ Week Five Workshop/subnet.tf
@@ -1,3 +1,4 @@
+
 resource "aws_subnet" "subnet_01_a" {
   vpc_id     = aws_vpc.vpc_01.id
   cidr_block = "10.0.1.0/24"
@@ -11,9 +12,14 @@ resource "aws_subnet" "subnet_01_b" {
   vpc_id     = aws_vpc.vpc_01.id
   cidr_block = "10.0.2.0/24"
  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
    tags = {
     Name = "subnet_01_b"
   }
+}
+resource "aws_route_table_association" "subnet_01_b_association" {
+  subnet_id      = aws_subnet.subnet_01_b.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 
